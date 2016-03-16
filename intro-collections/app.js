d9aa2b6f07736f153edb7dc368d48da0443971a2
oscar <oscaroceguera@gmail.com>
2016-03-16 10:56:42 -0700

    Collections - wrap

--- intro-collections/app.js
+++ intro-collections/app.js
@@ -16,6 +16,7 @@ Book = Backbone.Model.extend({
 
 var Library = Backbone.Collection.extend({
 	model:Book,
+  url: 'http://localhost:8080/books/',
 	initialize: function(){
 		this.on("remove",function(removedModel,models,options){
 			console.log("We removed model at index " + options.index);
@@ -36,38 +37,28 @@ var sampleBook = new Book({
 myLibrary.add(sampleBook);
 
 myLibrary.push({name:"The Alchemist",author:"Paulo Coehlo",year:"1993"});
-
 myLibrary.push({name:"Lord of Flies",author:"William Goodwing",year:"1953"});
 myLibrary.push({name:"Lord of Flies 2",author:"William Goodwing",year:"1953"});
 
-console.log('--- get id with cid  ---');
-console.log(myLibrary.get("c1"));
-
-console.log('--- Index ----');
-console.log(myLibrary.at(1));
-
-console.log('--- loop ---');
-for(var i = 0; i < myLibrary.length; i++){
-  console.log(myLibrary.at(i));
-  // console.log(myLibrary.at(i).get('name'));
-}
-
-console.log('--- Loop with underscore.js ---');
 myLibrary.forEach(function(model){
-  //console.log(model);
-  console.log(model.get('author'));
+  model.save()
 })
 
-console.log('--- pluck underscore---');
-console.log(myLibrary.pluck("name"));
+var myModel = new Book({
+  name: "Created and saved book",
+  author: "ahmed",
+  year: 2015
+})
 
-console.log('--- Where underscore ---');
-console.log(myLibrary.where({name:'The Alchemist'}));
-console.log(myLibrary.where({year:'1993'}));
+myLibrary.create(myModel)
 
-console.log('--- findwhere underscore ---');
-console.log(myLibrary.findWhere({name:"Lord of Flies"}));
-console.log(myLibrary.findWhere({year:"1953"}));
+myLibrary.fetch({
+  success: function(){
+    console.log('Collection retreived from the server');
+  },
+  error: function(){
+    console.log('An error happend');
+  }
+})
 
-console.log('--- GroupBy underscore ---');
-console.log(myLibrary.groupBy({year:'1953'}));
+console.log(myLibrary.models);
